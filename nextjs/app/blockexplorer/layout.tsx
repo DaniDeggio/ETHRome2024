5ffc754feab59fbb12f3b67ca0677c9294490ace
DanyDeggio <danideg12@gmail.com>
2024-10-06 04:44:35 +0200

    frontend testing

---
+++ nextjs/app/blockexplorer/layout.tsx
@@ -0,0 +1,9 @@
+import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+
+
+const BlockExplorerLayout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+
+export default BlockExplorerLayout;
