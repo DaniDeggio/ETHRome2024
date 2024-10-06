d10fb63907e184c2783c4eafdbd6b073c11e963d
DanyDeggio <danideg12@gmail.com>
2024-10-06 07:54:46 +0200

    cleaned

--- nextjs/app/blockexplorer/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
-
-
-
-const BlockExplorerLayout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
-};
-
-export default BlockExplorerLayout;
